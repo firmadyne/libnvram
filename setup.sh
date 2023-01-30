c8b334451f70eed91c9909b04ce63e63a90e5e20
Andrew Fasano <fasano@mit.edu>
2023-01-30 14:09:33 -0500

    Update setup script to use cross compiler paths in container

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ fi
 
 # Firmadyne cross compilers from https://zenodo.org/record/4922202
 # works for kernel 4.10
-CROSS_CC=/opt/cross/fd/${ARCH}-linux-musl${ABI}/bin/${ARCH}-linux-musl${ABI}-gcc
+CROSS_CC=/cross/${ARCH}-linux-musl${ABI}/bin/${ARCH}-linux-musl${ABI}-gcc
 
 
 make clean
